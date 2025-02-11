be43996e860c380f7ecdbda6a14294a1ea9413e0
deepblue <deepblue@inha.ac.kr>
2025-02-11 17:06:50 +0900

    v1.1 day02 prime number

--- day02.py
+++ day02.py
@@ -1,3 +1,9 @@
+# Assignment Day 02
+# v1.1) Change the for phrase to the while phrase.
+# v1.2) Write a program that receives two numbers and outputs only prime numbers between the two numbers. Then, use only the while statement and include the two numbers entered.
+# v1.3) Rewrite the code using the power function instead of the ** operator.
+# v1.4) Make my_pow custom function instead of ** operator, power function and make it work.
+
 def is_prime(num) -> bool:
     """
     A function that returns True if it is a prime number and False if it is not a prime number
@@ -5,19 +11,19 @@ def is_prime(num) -> bool:
     :return: boolean type
     """
     if num >= 2:
-        for i in range(2, int(num ** 0.5) + 1):
+        i = 2
+        while i < (int(num ** 0.5) + 1):
+        #for i in range(2, int(num ** 0.5) + 1):
             if num % i == 0:
                 return False
-                #is_prime = False
-                #break
-            #print(i, end=' ')
+            i = i + 1
     else:
         return False
     return True
 
 # main
 #help(abs)
-help(is_prime)
+#help(is_prime)
 n = int(input("Input number : "))
 
 if is_prime(n):  # function call
