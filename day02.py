977c221e3d8fbc89e53b9773b47969b6793b7ac4
deepblue <deepblue@inha.ac.kr>
2025-02-11 13:31:20 +0900

    v1.0 day02 prime number

--- day02.py
+++ day02.py
@@ -1,19 +1,26 @@
-# print(type(2**10))  # 1024
-# print(type(16**0.5))  # 4.0
-n = int(input("Input number : "))
-is_prime = True
-if n >= 2:
-    #for i in range(2, n):
-    for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            is_prime = False  #count = count + 1
-            break
-        print(i, end=' ')
-else:
-    is_prime = False
+def is_prime(num) -> bool:
+    """
+    A function that returns True if it is a prime number and False if it is not a prime number
+    :param num: integer number
+    :return: boolean type
+    """
+    if num >= 2:
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+                #is_prime = False
+                #break
+            #print(i, end=' ')
+    else:
+        return False
+    return True
 
-#if count == 0:
-if is_prime:
+# main
+#help(abs)
+help(is_prime)
+n = int(input("Input number : "))
+
+if is_prime(n):  # function call
     print(f"{n} is prime number")
 else:
     print(f"{n} is NOT prime number!")
