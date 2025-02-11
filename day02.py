8645ec3f70fb4346ba6cecf417c6294bba99a457
deepblue <deepblue@inha.ac.kr>
2025-02-11 10:33:27 +0900

    v0.4 day02 gugudan

---
+++ day02.py
@@ -0,0 +1,8 @@
+#dan = input("Input dan : ")
+dan = int(input("Input dan : "))
+for i in range(1, 10, 1):
+    print(f"{dan} * {i} = {dan*i}")
+
+# for dan in range(2, 10, 1):
+#     for i in range(1, 10, 1):
+#         print(f"{dan} * {i} = {dan*i}")
