ba06de6bfcadaa510ff9daddb578d12ebd06882d
woobin <guiyomyee@gmail.com>
2025-02-19 16:19:55 +0900

    day07

---
+++ day05.py
@@ -0,0 +1,44 @@
+# #from day02 import numbers
+#
+#
+# #def my_range(first=0, last=5, step=1):
+#  #   number = first
+#   #  while number <last:
+#    #     yield number
+#     #    number = number + step
+#
+# r = my_range()
+#
+# print(r,type(r))
+#
+# for x in r:
+#     print(x)
+# for x in r:
+#     print(x)
+#
+# def fibo(n):
+#     if n>2:
+#       return fibo(n-1)+fibo(n-2)
+#
+#     elif n==2:
+#         return 3
+#
+
+# 졸지마 졸지마 졸지마 졸지마 졸지마 졸지마 졸지마 졸지마
+
+
+class Pokemon:
+    def __init__(self, name):
+        self.name = name
+        print(f"{name} 포켓몬스터 생성")
+
+    def attack (self, target):
+        print(f"{self.name} 이 {target.name}을 공격!")
+
+
+charizard = Pokemon("리지웅")
+pikachu = Pokemon("피카쥬")
+squirtle = Pokemon("")
+
+# 그만 졸아 그만 졸아 그만 졸아 그만 졸아 그만 졸아 그만 졸아 그만 졸아 그만 졸아 그만 졸아 450타 가능 ??? 님 타자 몇 나옴 ?????
+
